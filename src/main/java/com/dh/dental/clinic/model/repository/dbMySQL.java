bdc2ad30d652eb5f0fc8c2971b8f33a286831419
MauriRodiez <c.maurodriguez.a@gmail.com>
2024-03-12 22:06:57 -0300

    Actualizando conexion a base de datos

--- src/main/java/com/dh/dental/clinic/model/repository/dbMySQL.java
+++ src/main/java/com/dh/dental/clinic/model/repository/dbMySQL.java
@@ -8,7 +8,7 @@ public class dbMySQL {
 
     private static dbMySQL instance;
     private Connection connection;
-    private static final String URL = "jdbc:mysql://localhost:3306/ . . . ";
+    private static final String URL = "jdbc:mysql://localhost:3306/Dental_clinic";
     private static final String USER = "root";
     private static final String PASSWORD = "Bdatos2023";
     private dbMySQL(){

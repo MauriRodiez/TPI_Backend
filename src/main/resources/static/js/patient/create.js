154ef1bb664ab34cefb0c642e4781c9b29077718
Maurirodiez <c.maurodriguez.a@gmail.com>
2024-03-25 16:36:24 -0300

    Avance de la vista en crear paciente

--- src/main/resources/static/js/patient/create.js
+++ src/main/resources/static/js/patient/create.js
@@ -1,5 +1,44 @@
 window.addEventListener('load', function(){
+    /*-----------------------------------------------*/
+    /*--                  Nodos                    --*/
+    /*-----------------------------------------------*/
+    const create = document.querySelector("#create");
+    const page = document.querySelector(".page-patient");
+    const box = document.querySelector(".box");
 
-const url = '/patient/create';
+    /*-----------------------------------------------*/
+    /*--                   Form                    --*/
+    /*-----------------------------------------------*/
+    const form = document.createElement("form");
+    form.id = "createPatient";
 
-})
+    // Input name
+    const nombreLabel = document.createElement("label");
+    nombreLabel.textContent = "Nombre:";
+
+    const nameInput = document.createElement("input");
+    nameInput.type = "text";
+    nameInput.name = "namePatient";
+
+    // Button cargar
+    const submitBtn = document.createElement('button');
+    submitBtn.type = 'submit';
+    submitBtn.textContent = 'Registrarse';
+
+    // Add elements to form
+    form.appendChild(nombreLabel);
+    form.appendChild(nameInput);
+    form.appendChild(submitBtn);
+    box.appendChild(form);
+
+    /*-----------------------------------------------*/
+    /*--           Evento click Agregar Paciente    --*/
+    /*-----------------------------------------------*/
+    create.addEventListener("click", function(event){
+        event.preventDefault();
+
+        page.style.display = "block";
+        page.innerHTML = '';
+        page.appendChild(box);
+    });
+});
---
+++ src/test/java/com/dh/dental/clinic/service/impl/DentistServiceTest.java
@@ -0,0 +1,33 @@
+package com.dh.dental.clinic.service.impl;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DentistServiceTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void save() {
+    }
+
+    @Test
+    void listAll() {
+    }
+
+    @Test
+    void searchById() {
+    }
+
+    @Test
+    void update() {
+    }
+
+    @Test
+    void delete() {
+    }
+}
